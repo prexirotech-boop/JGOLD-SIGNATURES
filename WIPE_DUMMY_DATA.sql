a927495978a869eba8c78e8272a2f918f8cbbac9
npreciousofficial-star <nprecious.official@gmail.com>
2026-08-01 15:16:25 +0100

    chore: update wipe data SQL script to safeguard admin@jgoldsignatures.com.ng

--- WIPE_DUMMY_DATA.sql
+++ WIPE_DUMMY_DATA.sql
@@ -12,10 +12,10 @@ TRUNCATE TABLE public.orders RESTART IDENTITY CASCADE;
 -- We safeguard admins by checking:
 --   a) role inside public.profiles is 'admin'
 --   b) raw_app_meta_data role is 'admin'
---   c) email is 'ebonyjuliet15@yahoo.com'
+--   c) email is 'admin@jgoldsignatures.com.ng' or 'ebonyjuliet15@yahoo.com'
 DELETE FROM auth.users 
 WHERE id NOT IN (
   SELECT id FROM public.profiles WHERE role = 'admin'
 )
 AND COALESCE(raw_app_meta_data->>'role', '') <> 'admin'
-AND email <> 'ebonyjuliet15@yahoo.com';
+AND email NOT IN ('admin@jgoldsignatures.com.ng', 'ebonyjuliet15@yahoo.com');
